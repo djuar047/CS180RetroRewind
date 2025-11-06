55ec80eb914c5855419452dbe79175a794e80b39
AlexC111013 <alexanderchavez1013@gmail.com>
2025-11-06 14:55:45 -0800

    Added frontend routing + pages (Login/Profile) and React Router setup

--- retrorewind-frontend/src/main.jsx
+++ retrorewind-frontend/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // lets us move between pages like Home, Login, and Profile
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+// This sets up the app and connects it to the "root" element in index.html.
+// We also wrap it with BrowserRouter so our app can use page routes.
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* BrowserRouter allows page navigation without reloading the whole site */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
+
